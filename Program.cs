9abeb279e723c638f82d848423947fe38cfc6a0f
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-04 21:18:55 +0300

    added dbcontext to services

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using UsersApi.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<UserContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
