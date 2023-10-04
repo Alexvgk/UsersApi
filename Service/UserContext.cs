63e66e38f83aa791188627c8e650be0e421c8a6c
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-04 21:08:54 +0300

    added context for db

---
+++ Service/UserContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using UsersApi.Model;
+
+namespace UsersApi.Service
+{
+    public class UserContext : DbContext
+    {
+
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> users { get; set; }
+        public DbSet<Role> roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // Пример: Конфигурация отношения многие-ко-многим между User и Role
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Roles)
+                .WithMany(r => r.Users)
+                .UsingEntity(j => j.ToTable("Roles"));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
