dbf8c55733f36e3bb15d0d5404072cdc0663c742
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-04 22:15:13 +0300

    added userRole entity for realization many-to-many relationship

---
+++ Model/UserRole.cs
@@ -0,0 +1,11 @@
+﻿namespace UsersApi.Model
+{
+    public class UserRole
+    {
+        public int userId { get; set; }
+        public User user { get; set; }
+
+        public int roleId { get; set; }
+        public Role role { get; set; }
+    }
+}
