6ae3f095f5dfdbb876635d523be81038d47e35a0
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-05 00:45:54 +0300

    full crud done

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using UsersApi.Model;
 using UsersApi.Service;
 
 namespace UsersApi.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
         private readonly UserContext _context;
@@ -15,8 +18,8 @@ namespace UsersApi.Controllers
         }
 
         //all users 
-        [HttpGet]
-        public async Task<IActionResult> GetUsers(int page = 1, int pageSize = 10, string sortBy = "Id", string sortOrder = "asc", string filter = "")
+        [HttpGet("/")]
+        public async Task<IActionResult> GetUsers(string? filter = "", int page = 1, int pageSize = 10, string sortBy = "Id", string sortOrder = "asc")
         {
             try
             {
@@ -49,22 +52,36 @@ namespace UsersApi.Controllers
                 var totalItems = await query.CountAsync();
                 var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
                 var currentPage = Math.Clamp(page, 1, totalPages);
-
-                var users = await query
-                    .Skip((currentPage - 1) * pageSize)
-                    .Take(pageSize)
-                    .ToListAsync();
-
-                var result = new
+                if (currentPage > 1)
                 {
-                    TotalItems = totalItems,
-                    TotalPages = totalPages,
-                    CurrentPage = currentPage,
-                    PageSize = pageSize,
-                    Users = users
-                };
-
-                return Ok(result);
+                    var users = await query
+                        .Skip((currentPage - 1) * pageSize)
+                        .Take(pageSize)
+                        .ToListAsync();
+                    var result = new
+                    {
+                        TotalItems = totalItems,
+                        TotalPages = totalPages,
+                        CurrentPage = currentPage,
+                        PageSize = pageSize,
+                        Users = users
+                    };
+                    return Ok(result);
+                }
+                else
+                {
+                    var users = await query
+                        .ToListAsync();
+                    var result = new
+                    {
+                        TotalItems = totalItems,
+                        TotalPages = totalPages,
+                        CurrentPage = currentPage,
+                        PageSize = pageSize,
+                        Users = users
+                    };
+                    return Ok(result);
+                }
             }
             catch (Exception ex)
             {
@@ -76,36 +93,171 @@ namespace UsersApi.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUserById(int id)
         {
-            return null;
-     
+            try
+            {
+                
+                var user = await _context.users
+                    .Include(u => u.userRoles) 
+                    .SingleOrDefaultAsync(u => u.Id == id);
+
+                if (user == null)
+                {
+                    return NotFound($"No one users with id{id}");
+                }
+
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+
         }
 
         //add new role for user
         [HttpPost("{userId}/roles")]
-        public async Task<IActionResult> AddRoleToUser(int userId, [FromBody] Role role)
+        public async Task<IActionResult> AddRoleToUser(int userId, [FromBody] string? bodyRole)
         {
-            return null;
+            try
+            {
+                
+                var user = await _context.users.Include(u => u.userRoles).FirstOrDefaultAsync(u => u.Id == userId);
+
+                if (user == null)
+                {
+                    return NotFound($"No one users with id{userId}");
+                }
+
+               
+                var role = await _context.roles.FirstOrDefaultAsync(r => r.Name == bodyRole);
+
+                if (role == null)
+                {
+                    return NotFound("No such role");
+                }
+
+                
+                if (!(user.userRoles.IsNullOrEmpty()) && user.userRoles.Any(ur => ur.roleId == role.Id))
+                {
+                    return BadRequest("This user has this role");
+                }
+
+                // new role
+                var userRole = new UserRole
+                {
+                    userId = user.Id,
+                    roleId = role.Id
+                };
+
+                _context.UserRoles.Add(userRole);
+
+             
+                await _context.SaveChangesAsync();
+
+                return Ok("Роль успешно добавлена пользователю.");
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
         }
 
         //add user
         [HttpPost]
         public async Task<IActionResult> CreateUser([FromBody] User user)
         {
-            return null;
+            try
+            {
+                if (user == null)
+                {
+                    return BadRequest("User can't be null");
+                }
+                if(string.IsNullOrEmpty(user.Email) || string.IsNullOrEmpty(user.Name))
+                {
+                    return BadRequest("Not all fields are filled in");
+                }
+                if(user.Age <= 0)
+                {
+                    return BadRequest("Not acceptable age");
+                }
+                var existingUser = await _context.users.FirstOrDefaultAsync(u => u.Email == user.Email);
+                if (existingUser != null)
+                {
+                    return Conflict("This email is alredy use");
+                }
+
+                _context.users.Add(user);
+                await _context.SaveChangesAsync();
+
+                //201 Created
+                return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
         }
 
         //update user information
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateUser(int id, [FromBody] User user)
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] User updatedUser)
         {
-            return null;
+            try
+            {
+                var user = await _context.users.FirstOrDefaultAsync(u => u.Id == id);
+
+                if (user == null)
+                {
+                    return NotFound("No user with this id");
+                }
+
+                // new data
+                user.Name = updatedUser.Name;
+                user.Age = updatedUser.Age;
+                user.Email = updatedUser.Email;
+
+            
+                await _context.SaveChangesAsync();
+
+                return Ok("Information updated");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
         }
 
         //delete user
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
-            return null;
+            try
+            {
+                var user = await _context.users
+                    .Include(u => u.userRoles)
+                    .FirstOrDefaultAsync(u => u.Id == id);
+
+                if (user == null)
+                {
+                    return NotFound($"No one users with id{id}");
+                }
+
+
+                _context.UserRoles.RemoveRange(user.userRoles);
+
+
+                _context.users.Remove(user);
+
+                await _context.SaveChangesAsync();
+
+                return NoContent(); //204 No Content 
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+
+            }
         }
 
     }
