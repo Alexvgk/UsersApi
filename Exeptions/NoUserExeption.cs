e1b77e650132f39ac73055a4a1f7fa7ba805d851
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-05 04:48:38 +0300

    added user exception

---
+++ Exeptions/NoUserExeption.cs
@@ -0,0 +1,10 @@
+﻿namespace UsersApi.Exeptions
+{
+    public class NoUserExeption : Exception
+    {
+        public override string Message { get;}
+        public NoUserExeption(string message) {
+            this.Message = message;
+        }
+    }
+}
