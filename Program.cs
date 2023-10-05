53dceb4c0d984236f73eff86e204a54710795131
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-05 04:46:23 +0300

    added DI for repo and paging

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using UsersApi.Repository;
 using UsersApi.Service;
+using EntityFrameworkCore.UseRowNumberForPaging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<UserContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddDbContext<UserContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), builder => builder.UseRowNumberForPaging()));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
