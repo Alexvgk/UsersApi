e5406e64f1abe46e07f7cf4e2e11a4e531bc9d21
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-05 19:05:08 +0300

    added documentation for controller

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Annotations;
 using UsersApi.Helpers;
 using UsersApi.Model;
 using UsersApi.Repository;
 
 namespace UsersApi.Controllers
 {
-
+    /// <summary>
+    /// контроллер получения токена
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class TokenController : Controller
@@ -14,12 +17,26 @@ namespace UsersApi.Controllers
         private readonly IUserRepository _userRepository;
         private readonly IJwtService _jwtService;
 
+        /// <summary>
+        /// конструктор контроллера
+        /// </summary>
         public TokenController(IUserRepository userRepository, IJwtService jwtService)
         {
             _userRepository = userRepository;
             _jwtService = jwtService;
         }
+
+
+        /// <summary>
+        /// Получение токена
+        /// </summary>
+        /// <param name=_userData">пользователь, который получает токен</param>
+        /// <returns></returns>
         [HttpPost]
+        [SwaggerResponse(StatusCodes.Status200OK, "Get token",typeof(string))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User not found")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, "invalid credetials")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal server error")]
         public async Task<IActionResult> Post(User _userData){
             if (_userData != null && _userData.Email != null && _userData.Name != null)
             {
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Annotations;
 using UsersApi.Exeptions;
 using UsersApi.Model;
 using UsersApi.Repository;
@@ -10,39 +11,73 @@ using UsersApi.Service;
 
 namespace UsersApi.Controllers
 {
+
+    /// <summary>
+    /// main контроллер
+    /// </summary>
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : Controller
     {
         private readonly IUserRepository _userRepository;
+        private readonly ILogger<UserController> _logger;
 
-        public UserController(IUserRepository userRepository)
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        public UserController(IUserRepository userRepository, ILogger<UserController> logger)
         {
             _userRepository = userRepository;
+            _logger = logger;
         }
 
-        //all users 
+        /// <summary>
+        /// Получение всех пользователей
+        /// </summary>
+        /// <param name="filter">фильтр, позволяет отбирать данные по имени, эл.почте или роли</param>
+        /// <param name="page">страница вывода</param>
+        /// <param name="pageSize">размер страницы</param>
+        /// <param name="sortBy">сортировка по параметрам(id,name,email)</param>
+        /// <param name="sortOrder">порядок сортировки</param>
+        /// <returns></returns>
         [HttpGet("/")]
+        [SwaggerResponse(StatusCodes.Status200OK, "User list", typeof(List<User>))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User not found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal server error")]
         public async Task<IActionResult> GetUsers(string? filter = "", int page = 1, int pageSize = 10, string sortBy = "Id", string sortOrder = "asc")
         {
             try {
                 var user = await _userRepository.getUser(filter, page, pageSize, sortBy, sortOrder);
+                _logger.LogInformation("GET USERS");
                 return Ok(user.Users);
+            }
+            catch(ArgumentNullException ex)
+            {
+                return StatusCode(404, ex.Message);
             }
             catch (Exception ex)
             {
+                _logger.LogError("Exception in getting users");
                 return StatusCode(500, ex.Message);
             }
         }
-
+        /// <summary>
+        /// Поиск пользователя по id
+        /// </summary>
+        /// <param name="id">id пользователя</param>
+        /// <returns></returns>
         //get users by id and his roles
         [HttpGet("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, "User details", typeof(User))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User not found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal server error")]
         public async Task<IActionResult> GetUserById(int id)
         {
             try
             {
                 var user = await _userRepository.GetUserById(id);
+                _logger.LogInformation("GET USER BY ID");
                 return Ok(user);
             }
             catch(ArgumentNullException ex)
@@ -56,15 +91,24 @@ namespace UsersApi.Controllers
 
         }
 
-        //add new role for user
+        /// <summary>
+        /// Добавление роли пользователю
+        /// </summary>
+        /// <param name="userId">id пользователя</param>
+        /// <param name="bodyRole">роль, котоую хотим добавить(Admin,User,Support,SuperAdmin)</param>
+        /// <returns></returns>
         [HttpPost("{userId}/roles")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Role added")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User not found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal server error")]
         public async Task<IActionResult> AddRoleToUser(int userId, [FromBody] string? bodyRole)
         {
             try
             {
                 var resp = await _userRepository.AddRoleToUser(userId, bodyRole);
 
-                return Ok("Роль успешно добавлена пользователю.");
+                _logger.LogInformation($"ADDED ROLE TO USER {userId}");
+                return Ok("Role added.");
             }
             catch (ArgumentNullException ex) 
             {
@@ -72,7 +116,7 @@ namespace UsersApi.Controllers
             }
             catch (InvalidOperationException ex)
             {
-                return StatusCode(407, ex.Message);
+                return StatusCode(406, ex.Message);
             }
             catch (Exception ex)
             {
@@ -81,27 +125,37 @@ namespace UsersApi.Controllers
             }
         }
 
-        ////add user
+        /// <summary>
+        /// Добавление пользователя
+        /// </summary>
+        /// <param name="user">пользователь</param>
+        /// <returns></returns>
         [HttpPost]
+        [SwaggerResponse(StatusCodes.Status200OK, "User Created")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "no user")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "invalid data")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal server error")]
         public async Task<IActionResult> CreateUser([FromBody] User user)
         {
             try
             {
-                int userId = _userRepository.CreateUser(user).Result;
+                int userId = await _userRepository.CreateUser(user);
+                _logger.LogInformation($"CREATE USER WITH ID {userId}");
                 return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
             }
 
-            catch(ArgumentNullException ex)
+
+            catch (ArgumentNullException ex)
             {
                 return StatusCode(404, ex.Message);
             }
-            catch(ArgumentException ex)
+            catch (ArgumentException ex)
             {
-                return StatusCode(407, ex.Message);
+                return StatusCode(400, ex.Message);
             }
-            catch(InvalidOperationException ex)
+            catch (InvalidOperationException ex)
             {
-                return StatusCode(407, ex.Message);
+                return StatusCode(406, ex.Message);
             }
             catch (Exception ex)
             {
@@ -109,18 +163,27 @@ namespace UsersApi.Controllers
             }
         }
 
-        //update user information
+        /// <summary>
+        /// Добавление роли пользователю
+        /// </summary>
+        /// <param name="id">id пользователя, которому обновляем информацию/</param>
+        /// <param name="updatedUser">новые данные пользователя</param>
+        /// <returns></returns>
         [HttpPut("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, "User updated")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User not found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal server error")]
         public async Task<IActionResult> UpdateUser(int id, [FromBody] User updatedUser)
         {
             try
             {
               var resp = await _userRepository.UpdateUser(id, updatedUser);
-              return Ok(resp);
+                _logger.LogInformation($"USERS {id} UPDATED");
+                return Ok(resp);
             }
             catch(ArgumentNullException ex)
             {
-                return StatusCode(404, $"Internal server error: {ex}");
+                return StatusCode(404, ex.Message);
             }
             catch (Exception ex)
             {
@@ -128,17 +191,49 @@ namespace UsersApi.Controllers
             }
         }
 
-        //delete user
+        /// <summary>
+        /// удаление пользовалетя
+        /// </summary>
+        /// <param name="id">id пользователя, которого хотим удалить/</param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, "delete user")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User not found")]
         public async Task<IActionResult> DeleteUser(int id)
         {
             try
             {
                 bool resp = await _userRepository.DeleteUser(id);
+                _logger.LogInformation($"DELETE USER {id}");
                 return NoContent(); //204 No Content 
             }
             catch(NoUserExeption e){ return StatusCode(404, e.Message); }
             catch (Exception ex){ return StatusCode(500, $"Internal server error: {ex}"); }
+
+        }
+
+
+        /// <summary>
+        /// удаление роли у пользователя
+        /// </summary>
+        /// <param name="userId">id пользователя,у которого хотим удалить роль/</param>
+        /// <param name="role">роль,которую хотим удалить/</param>
+        /// <returns></returns>
+        [HttpDelete("{userId}/roles")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, "delete role")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "User not found")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Role not found")]
+        public async Task<IActionResult> DeleteRoleUser(int userId,string role)
+        {
+            try
+            {
+                bool resp = await _userRepository.DeleteRoleUser(userId,role);
+                _logger.LogInformation($"DELETE USER {userId}");
+                return NoContent(); //204 No Content 
+            }
+            catch (NoUserExeption e) { return StatusCode(404, e.Message); }
+            catch(ArgumentNullException e) { return StatusCode(404, e.Message); }
+            catch (Exception ex) { return StatusCode(500, $"Internal server error: {ex}"); }
 
         }
 
