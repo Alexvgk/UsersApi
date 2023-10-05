761a1c880a002c59f9f0a21e3c723b82faef79cc
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-05 19:06:27 +0300

    added some services to conteiner, set swagger settings

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using UsersApi.Helpers;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Logging.Configuration;
+using Serilog;
+using Serilog.Events;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,14 +32,27 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
         };
     });
+
+
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IJwtService, JwtService>();
 builder.Services.AddDbContext<UserContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), builder => builder.UseRowNumberForPaging()));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 builder.Services.AddSwaggerGen(c =>
 {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "UsersApi",
+        Version = "v1",
+        Description = "web-api"
+    });
+    var basePath = AppContext.BaseDirectory;
+
+    var xmlPath = Path.Combine(basePath, "docs.xml");
+    c.IncludeXmlComments(xmlPath);
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Description = "JWT Authorization header using the Bearer.",
@@ -52,13 +70,26 @@ builder.Services.AddSwaggerGen(c =>
         }
     });
 });
+
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "UsersApi v1");
+        c.RoutePrefix = "swagger";
+    }); ;
 
 }
 
