8992767036c0dd67d7aacbc18ed02ad815f1f682
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-05 04:47:35 +0300

    added class for switching data from repo to controller

---
+++ Model/UsersPagedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace UsersApi.Model
+{
+    public class UsersPagedResult
+    {
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public List<User>? Users { get; set; }
+    }
+}
