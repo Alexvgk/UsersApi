b89d943da5f6d21ec7f3489b3b303c1d572b4edb
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-06 20:06:32 +0300

    added Dto of user

---
+++ Model/Dto/DtoUser.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersApi.Model.Dto
+{
+    /// <summary>
+    /// Dto для создания пользователя, редактирования данных пользователя и получения токена
+    /// </summary>
+    public class DtoUser
+    { 
+        /// <summary> соответствует name из User </summary>
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
+        /// <summary> соответствует age из User </summary>
+        [Required(ErrorMessage = "Age is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Age must be a positive number")]
+        public int? Age { get; set; }
+
+        /// <summary> соответствует email из User  </summary>
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string? Email { get; set; }
+    }
+}
