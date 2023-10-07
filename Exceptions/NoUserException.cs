978a16d55f95a016af6fd892665c8b71a32a320d
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-10-07 13:16:43 +0300

    fixed name

---
+++ Exceptions/NoUserException.cs
@@ -0,0 +1,23 @@
+﻿namespace UsersApi.Exeptions
+{
+
+
+    /// <summary>
+    /// исключение выплывающее при отсутствии пользователя по заданым критериям
+    /// </summary>
+    public class NoUserException : Exception
+    {
+
+        /// <summary>
+        /// сообщение об ошибке
+        /// </summary>
+        public override string Message { get;}
+
+        /// <summary>
+        /// конструктор исключения
+        /// </summary>
+        public NoUserException(string message) {
+            this.Message = message;
+        }
+    }
+}
